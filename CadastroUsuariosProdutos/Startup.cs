0645029f81fb8113956775b1b882557115356485
heitor-bittencourt <heitor.g.bittencourt@gmail.com>
2021-02-22 17:57:59 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroUsuariosProdutos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CadastroUsuariosProdutos.Startup))]
+namespace CadastroUsuariosProdutos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
